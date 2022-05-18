7421ad1f13fb3b057c618b5e06fc5378ce58e9bd
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-18 19:52:03 +0000

    features/cookies implementation has been merged to the master

--- express_server.js
+++ express_server.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser");
+const { application } = require("express");
+//const { parseForESLint } = require("babel-eslint");
+
+app.use(cookieParser())
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,51 +18,61 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
+  
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: "" };
+  console.log("abc")
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString() {
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id
+  const longURL = req.body.newlongurl
+  urlDatabase[shortURL] = longURL
+  res.redirect("/urls")
+})
 
-}
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params)
+  const shortURL = req.params.shortURL
+  console.log(shortURL)
+  delete urlDatabase[shortURL]
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+    
+/* function generateRandomString() {
+
+} */ 
